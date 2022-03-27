5b203177d4f708ce835f6215a9e0e3d0965cd9aa
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2022-03-27 22:43:44 +0500

    Update comment for Cache interface (#13)

--- cache.go
+++ cache.go
@@ -1,6 +1,6 @@
 package lru
 
-// Cache defines minimal interface for all cache implementations, that requires two methods: Get and Set.
+// Cache defines minimal interface for all cache implementations.
 type Cache[K comparable, V any] interface {
 	Get(key K) *V
 
